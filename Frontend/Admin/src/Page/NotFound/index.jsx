64e60bfb46e5c9f1a6465e41b6fc62f29baedbf7
Bùi Quốc Huy <huy01682312920@gmail.com>
2022-01-01 21:26:18 +0700

    notfound

--- Frontend/Admin/src/Page/NotFound/index.jsx
+++ Frontend/Admin/src/Page/NotFound/index.jsx
@@ -1,14 +1,21 @@
-import React, { useEffect } from "react";
-
+import React, { memo, useEffect } from "react";
+import { Link } from 'react-router-dom';
+import "./styles.scss"
 function NotFound() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   return (
-    <div>
-      <h1>Not Found</h1>
-    </div>
+<div className="NotFound">
+ <b><Link to={"/"} className="goback">Về trang chủ</Link></b>
+    <img
+        src="https://images.all-free-download.com/images/graphiclarge/error_404_page_not_found_6845510.jpg"
+        alt=""
+        className="img-notfound"
+        
+      />
+</div>
   );
 }
 
-export default NotFound;
+export default memo(NotFound);
